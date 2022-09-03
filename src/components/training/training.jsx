9559570d9437656428769aaabce3da8a8f53f977
ArturoJCastilloZ <castillo.arturo93@hotmail.com>
2022-09-03 13:22:24 -0500

    Cambio en la estructura de componentes de Training

--- src/components/training/training.jsx
+++ src/components/training/training.jsx
@@ -1,43 +1,15 @@
 import React from 'react';
-import '../../styles/components/training/training.scss';
+import Jobs from './jobs/jobs';
+import Studies from './studies/studies';
+import '../../styles/components/training/jobs.scss';
+import '../../styles/components/training/studies.scss';
 
 const Training = ({studies, jobs}) => {
     return (
-        <div>
-            <h1 className='studies-title-header'>Studies &darr;</h1>
-            <div className='studies'>
-                {studies.map((studies, index) => {
-                    return (
-                        <div className='studies-card' key={studies.title + index}>
-                                <p className='studies-card-title'>{studies.title}</p>
-                                <hr />
-                                <p className='studies-card-career'>{studies.career}</p>
-                                <hr />
-                                <p className='studies-card-institution'>{studies.institution}</p>
-                                <hr />
-                                <p className='studies-card-institution'>{studies.date}</p>
-                        </div>
-                    )
-                })}
-            </div>
-            <hr />
-            <h1 className='jobs-title-header'>Experience &darr;</h1>
-            <div className='jobs'>
-                {jobs.map((jobs, index) => {
-                    return (
-                        <div className='jobs-card' key={jobs.title + index}>
-                                <p className='jobs-card-title'>{jobs.title}</p>
-                                <hr />
-                                <p className='jobs-card-career'>{jobs.institution}</p>
-                                <hr />
-                                <p className='jobs-card-institution'>{jobs.date}</p>
-                                <hr />
-                                <p className='jobs-card-institution'>{jobs.activity}</p>
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
+        <section>
+            <Jobs jobs={jobs}/>
+            <Studies studies={studies}/>
+        </section>
     );
 }
 
