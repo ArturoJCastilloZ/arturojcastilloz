947bedd6d717b04dbc9ff7b617b020f0620d2a68
ArturoJCastilloZ <castillo.arturo93@hotmail.com>
2023-06-07 21:35:29 -0600

    Tootlip icons footer

--- src/containers/footer/content/content.jsx
+++ src/containers/footer/content/content.jsx
@@ -1,13 +1,15 @@
-import * as Icons from 'react-icons/fa'
+import * as Icons from 'react-icons/fa';
+import { Tooltip } from 'react-tooltip';
 
-const FooterContent = ({ icon, url, color }) => {
+const FooterContent = ({ icon, url, color, title }) => {
     if (!Icons[icon]){
         throw new Error(`Icono invalido: ${icon}`)
     }
     const Icon = Icons[icon]
   return (
     <li className='footer-item' style={{'--color': color, "cursor": "pointer" }}>
-        <a href={url}><Icon /></a>
+        <a href={url} data-tooltip-id={url} data-tooltip-content={title} data-tooltip-place="top"><Icon /></a>
+        <Tooltip id={url} />
     </li>
   )
 }
