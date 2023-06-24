a2d5d8a652c831fd9092cf5b7df0eb83b4dc9f4e
ArturoJCastilloZ <castillo.arturo93@hotmail.com>
2023-06-23 18:59:37 -0600

    Se agrega botón de Ver más a la descripción de empleos

--- src/containers/jobs/content/content.jsx
+++ src/containers/jobs/content/content.jsx
@@ -1,16 +1,37 @@
+import React, { useState } from 'react';
+
 const JobsContent = ({ start, end, title, institution, activity }) => {
+  const maxLength = 140; // Número máximo de caracteres permitidos
+  const [showFullText, setShowFullText] = useState(false);
+
+  const handleToggleText = () => {
+    setShowFullText(!showFullText);
+  };
+
+  let displayedText = activity;
+  let buttonText = 'Ver más...';
+
+  if (!showFullText && activity.length > maxLength) {
+    displayedText = activity.substring(0, maxLength) + '...';
+  } else {
+    buttonText = 'Ver menos';
+  }
+
   return (
     <div className='jobs-items-item' data-aos="zoom-in">
-        <div className='jobs-dot'></div>
-        <div className='jobs-items-item-date'>{start} - {end}</div>
-        <div className='jobs-content'>
-            {/* <img src={jobs.img} alt={jobs.institution} /> */}
-            <h3>{title}</h3>
-            <span>{institution}</span>
-            <p>{activity}</p>
-        </div>
+      <div className='jobs-dot'></div>
+      <div className='jobs-items-item-date'>{start} - {end}</div>
+      <div className='jobs-content'>
+        {/* <img src={jobs.img} alt={jobs.institution} /> */}
+        <h3>{title}</h3>
+        <span>{institution}</span>
+        <p>{displayedText}</p>
+        {activity.length > maxLength && (
+          <button className="button" onClick={handleToggleText}>{buttonText}</button>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export {JobsContent}
+export { JobsContent };
